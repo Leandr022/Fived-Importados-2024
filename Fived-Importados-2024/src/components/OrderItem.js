806c99f77b7e7ec63e8b2bfacd5ddd2b53d046e4
Leandr022 <leandro.santagada@gmail.com>
2024-09-27 22:17:23 -0300

    cambios en orderitem

--- Fived-Importados-2024/src/components/OrderItem.js
+++ Fived-Importados-2024/src/components/OrderItem.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable,StyleSheet, Text, View } from 'react-native'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { colors } from '../global/colors';
 import { useNavigation } from '@react-navigation/native';
